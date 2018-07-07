0847bf77dfbd0128568303e859b39c883c41012f
arvi2001tcs <40921578+arvi2001tcs@users.noreply.github.com>
2018-07-08 00:51:52 +0530

    Update App.spec.js

--- frontend/test/unit/specs/App.spec.js
+++ frontend/test/unit/specs/App.spec.js
@@ -2,7 +2,20 @@ import App from '@/App'
 
 describe('App.vue', () => {
   it('should be initialized with proper values', () => {
-    const items = []
+    const items = [{
+        path: 'flight',
+        icon: 'fas fa-plane',
+        title: 'Flights'
+      }, {
+        path: 'passenger',
+        icon: 'fas fa-user-circle',
+        title: 'Passengers'
+      }, {
+        path: 'reservation',
+        icon: 'fas fa-clipboard-check',
+        title: 'Reservations'
+      }]
+    
 
     expect(App.data().clipped).to.equal(false)
     expect(App.data().drawer).to.equal(false)
@@ -11,6 +24,6 @@ describe('App.vue', () => {
     expect(App.data().right).to.equal(true)
     expect(App.data().rightDrawer).to.equal(false)
     expect(App.data().title).to.equal('Book your flight')
-    expect(App.data().items).to.be.deep.equal(3)
+    expect(App.data().items).to.be.deep.equal(items)
   })
 })
